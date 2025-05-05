1bf898ca098c9142dcaf001a307484026a9d563a
Seb4sC <sacastro081@soy.sena.edu.co>
2025-05-05 11:35:59 -0500

    - Docker compose file for postgres database - Prisma for database management - Next config for interval polling updates in real time on windows - Better README file - Api GET test method

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  watchOptions: {
+    pollIntervalMs: 1000,
+  },
 };
 
 export default nextConfig;
