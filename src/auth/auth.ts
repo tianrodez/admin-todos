139201cc7da9c0191b29baf79ce46962492eced2
Seb4sC <sacastro081@soy.sena.edu.co>
2025-06-20 23:23:24 -0500

    Update dependencies and Prisma schema
    - Updated @prisma/client and prisma versions to ^6.10.1 in package.json and package-lock.json.
    - Added prisma/migrations to .gitignore to prevent migration files from being tracked.
    - Updated schema.prisma to include User, Account, Session, and VerificationToken models for authentication.
    - Modified auth.ts to use the PrismaAdapter with the updated prisma instance.

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,7 +1,10 @@
+import prisma from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub, Google],
 });
